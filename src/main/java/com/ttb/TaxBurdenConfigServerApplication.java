258bc5d12a4472f6314e66cc666e56d5b5d66aeb
Brian Folse <bmfolse@gmail.com>
2017-05-04 17:33:15 -0400

    initial

---
+++ src/main/java/com/ttb/TaxBurdenConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.ttb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxBurdenConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxBurdenConfigServerApplication.class, args);
+	}
+}
